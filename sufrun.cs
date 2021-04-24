f2dc0cc9779a9483501f740296e77ed82e756501
Melon <60455005+berkayberatsonmez@users.noreply.github.com>
2021-04-24 20:02:41 +0300

    Create sufrun.cs

---
+++ sufrun.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sufrun
+{
+    class Araba
+    {
+        public float Akim { get; set; }
+        public float Hiz { get; set; }
+        public float Maxsic { get; set; }
+        public float Topger { get; set; }
+        public float Sic1 { get; set; }
+        public float Sic2 { get; set; }
+        public float Sic3 { get; set; }
+        public int Pil1 { get; set; }
+        public int Pil2 { get; set; }
+        public int Pil3 { get; set; }
+        public int Pil4 { get; set; }
+        public int Pil5 { get; set; }
+        public int Pil6 { get; set; }
+        public int Pil7 { get; set; }
+        public int Pil8 { get; set; }
+        public int Pil9 { get; set; }
+        public int Pil10 { get; set; }
+        public int Pil11 { get; set; }
+        public int Pil12 { get; set; }
+        public int Pil13 { get; set; }
+        public int Pil14 { get; set; }
+        public int Pil15 { get; set; }
+        public int Pil16 { get; set; }
+        public int Pil17 { get; set; }
+        public int Pil18 { get; set; }
+        public int Pil19 { get; set; }
+        public int Pil20 { get; set; }
+
+    }
+}
